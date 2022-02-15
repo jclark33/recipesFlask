92d13ede92c80f7d9f7c8f6989415709ded5af55
Julia <jlclark024@gmail.com>
2022-02-15 21:30:09 +0000

    Added new page

--- flask_app.py
+++ flask_app.py
@@ -25,6 +25,11 @@ class Recipe(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(4096))
 
+@app.route("/addnew", methods=["GET","ADD"])
+def addnew():
+    if request.method == "GET":
+        # YOU LEFT OFF HERE GENIUS
+
 @app.route("/", methods=["GET","POST"])
 def index():
     if request.method == "GET":
